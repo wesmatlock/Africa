7a61d7c38adea5f5fe72db38079b8a68d2a33b41
wesley matlock <wesley.matlock@mlb.com>
2021-01-28 07:51:38 -0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Wesley Matlock on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
