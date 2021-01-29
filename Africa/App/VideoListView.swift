93c4d5a497e579fdda16cf0f28cd2760b3eb73c4
wesley matlock <wesley.matlock@mlb.com>
2021-01-29 13:55:16 -0700

    Videos!!

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -1,13 +1,40 @@
 import SwiftUI
 
 struct VideoListView: View {
+
+  // MARK: - Properties
+  @State var videos: [Video] = Bundle.main.decode("videos.json")
+  let hapticImapact = UIImpactFeedbackGenerator(style: .medium)
+
+  // MARK: - Body
   var body: some View {
-    Text("VideoListView")
+    NavigationView {
+      List {
+        ForEach(videos) { item in
+          VideoListItemView(video: item)
+            .padding(.vertical, 8)
+        }//: Loop
+      }//: List
+      .listStyle(InsetGroupedListStyle())
+      .navigationBarTitle("Videos", displayMode: .inline)
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          Button(action: {
+            videos.shuffle()
+            hapticImapact.impactOccurred()
+          }) {
+            Image(systemName: "arrow.2.squarepath")
+          }
+        }
+      }
+    }//: Navigation
   }
 }
 
+// MARK: Preview
 struct VideoListView_Previews: PreviewProvider {
   static var previews: some View {
     VideoListView()
+      .previewDevice("iPhone 12 Pro")
   }
 }
