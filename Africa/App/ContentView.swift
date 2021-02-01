b34881c05524f1e538879555a3296f615af19bef
wesley matlock <wmatlock@gmail.com>
2021-02-01 08:22:54 -0700

    Mac App!

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -49,6 +49,8 @@ struct ContentView: View {
                 AnimalListItemView(animal: animal)
               }//: Link
             }//: Loop
+            CreditsView()
+              .modifier(CenterModifier())
           }//: List
         } else {
           ScrollView(.vertical, showsIndicators: false) {
